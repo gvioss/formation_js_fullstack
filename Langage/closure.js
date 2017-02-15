b0108f607bfee809ae1a6341c02b69e188463abc
Geoffray Viossat <geoffray.viossat@gmail.com>
2017-02-15 10:03:21 +0100

    Jour 1

--- Langage/closure.js
+++ Langage/closure.js
@@ -1,3 +1,33 @@
-/**
- * Created by Administrateur on 15/02/2017.
- */
+var logClosure = function(msg) {
+    // portée de closure (sauvegardée)
+    return function() {
+        return msg;
+    };
+};
+
+var logBonjour = logClosure('Bonjour');
+console.log(logBonjour());
+
+var logCoucou = logClosure('Coucou');
+console.log(logCoucou());
+
+var timeoutClosure = function() {
+    setTimeout(function() {
+
+    }, 1000);
+};
+
+var prenoms = ['Romain', 'Jean'];
+
+for (var i=0; i<prenoms.length; i++) {
+    var prenom = prenoms[i];
+    setTimeout(function() {
+        console.log(prenom);
+    }, 1000);
+}
+
+prenoms.forEach(function(prenom) {
+    setTimeout(function() {
+        console.log(prenom);
+    }, 1000);
+});
--- Langage/jeu-es6.js
+++ Langage/jeu-es6.js
@@ -1,3 +1,72 @@
-/**
- * Created by Administrateur on 15/02/2017.
- */
+// Ex : Jeu du plus ou moins
+// 1- Générer un entier aléatoire entre 0 et 100 inclus (API Math sur MDN)
+// 2- Demander et récupérer la saisie d'un entier (API readline de Node)
+// et afficher si l'entier recherché est plus grand, plus petit ou trouvé
+// 3- Pouvoir trouver en plusieurs tentatives
+// 4- Stocker chaque tentative dans un tableau et les réafficher entre chaque tour
+// (API Array sur MDN)
+// 5- Afficher une erreur et rejouer si la saisie n'est pas un nombre
+const readline = require('readline');
+
+const tentatives = [];
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+class Jeu {
+    constructor(options = {}) {
+        options.min = options.min || 0;
+        options.max = options.max || 100;
+        this._nbAlea = getRandomIntInclusive(options.min, options.max);
+        this._essais = [];
+    }
+    jouer() {
+        if (this._essais.length) {
+            console.log(`Vous avez déjà joué : ${this._essais.join(' - ')}`);
+        }
+
+        rl.question('Quel est le nombre ? ', saisie => {
+            var entierSaisi = Number.parseInt(saisie);
+
+            if (Number.isNaN(entierSaisi)) {
+                console.log('Il faut saisir un nombre');
+                return this.jouer();
+            }
+
+            this._essais.push(entierSaisi);
+
+            if (entierSaisi < this._nbAlea) {
+                console.log(`${entierSaisi} est trop petit`);
+                return this.jouer();
+            }
+
+            if (entierSaisi > this._nbAlea) {
+                console.log(`${entierSaisi} est trop grand`);
+                return this.jouer();
+            }
+
+            console.log(`C'est gagné !`);
+            rl.close();
+        });
+    }
+}
+
+
+
+// Utilisation
+var jeu = new Jeu({
+    max: 50
+});
+
+jeu.jouer();
