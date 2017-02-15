693efc2f1b004bf3d62cf050928e5d5992cede58
Geoffray Viossat <geoffray.viossat@gmail.com>
2017-02-15 15:26:09 +0100

    Jour 2

---
+++ AdressBook/client/horloge.js
@@ -0,0 +1,13 @@
+import moment from 'moment';
+
+export default class Horloge {
+    constructor(containerElt) {
+        this.containerElt = containerElt;
+    }
+    update() {
+        this.containerElt.innerHTML = moment().format('HH:mm:ss');
+    }
+    start() {
+        setInterval(this.update.bind(this), 100)
+    }
+}
