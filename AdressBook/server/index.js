4706e5008e6837d2775fcc8ac2fe400513e03814
Geoffray Viossat <geoffray.viossat@gmail.com>
2017-02-16 12:04:36 +0100

    Jour 3

--- AdressBook/server/index.js
+++ AdressBook/server/index.js
@@ -1,27 +1,10 @@
 const express = require('express');
+const contact = require('./route/contact');
 
-const server = express();
+const app = express();
 
-const contacts = [{
-    prenom: 'Jean',
-    nom: 'Dupont',
-    id: 1
-},{
-    prenom: 'Eric',
-    nom: 'Martin',
-    id: 2
-}];
+app.use('/api/contacts', contact);
 
-server.get('/api/contacts', function(req, res) {
-    res.json(contacts);
-});
-
-server.get('/api/contacts/:id', function(req, res) {
-    var id = Number(req.params.id);
-    var contact = contacts.find(c => id === c.id);
-    res.json(contact);
-});
-
-server.listen(8080, function(){
+app.listen(8080, function(){
     console.log('Server listening');
 });
