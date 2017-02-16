87f7fb14af9bb2a12e8e9d3217637b7f8d724163
Geoffray Viossat <geoffray.viossat@gmail.com>
2017-02-16 11:09:42 +0100

    Jour 3

--- Node.js/hello-express.js
+++ Node.js/hello-express.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const server = express();
 
-server.all('/', function(req, res) {
+server.use(morgan('dev'));
+
+/*
+server.use(function(req, res, next) {
+    console.log(req.method + ' ' + req.url);
+    next();
+});
+*/
+
+server.use(function(req, res, next) {
+    res.setHeader('Content-type','text/plain');
+    next();
+});
+
+server.get('/', function(req, res) {
     res.send('Hello');
 });
 
-server.all('/toto', function(req, res) {
+server.get('/toto', function(req, res) {
     res.send('Hello Toto !!!');
+});
+
+server.use(function(req, res, next) {
+    res.statusCode = 404;
+    res.send('Erreur 404');
 });
 
 server.listen(8080, function(){
